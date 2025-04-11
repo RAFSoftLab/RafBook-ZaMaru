5d3b3c1ffb1fb5718b995b51f86c81445435ff9b
Ivana <ivanajankovic1302@gmail.com>
2025-04-11 15:35:26 +0200

    zavrseni testovi

--- src/test/java/ApiChannelTest.java
+++ src/test/java/ApiChannelTest.java
@@ -37,55 +37,58 @@ public class ApiChannelTest extends ApplicationTest {
         helloApp.start(stage);
     }
 
+    @Test
+    public void testAddChannel() {
+        TextField usernameField = lookup("#usernameField").query();
+        PasswordField passwordField = lookup("#passwordField").query();
+        Button loginButton = lookup("#loginButton").queryButton();
 
-//    @Test
-//    public void testAddChannel() {
-//
-//        TextField usernameField = lookup("#usernameField").query();
-//        PasswordField passwordField = lookup("#passwordField").query();
-//        Button loginButton = lookup("#loginButton").queryButton();
-//
-//        clickOn(usernameField).write("mara");
-//        clickOn(passwordField).write("mara123");
-//
-//        clickOn(loginButton);
-//
-//        TabPane tabPane = lookup("#tabPane").query();
-//        tabPane.getSelectionModel().select(1);
-//
-//        TextField nameField = (TextField) lookup("#name").nth(0).query();
-//        TextField descriptionField = (TextField) lookup("#description").nth(1).query();
-//
-//        clickOn(nameField).write("Novi Kanal");
-//        clickOn(descriptionField).write("Opis novog kanala");
-//
-//        ComboBox<String> comboBox1 = lookup("#comboBox1").query();
-//        clickOn(comboBox1);
-//        comboBox1.getSelectionModel().select(0);
-//
-//        ComboBox<String> comboBox2 = lookup("#comboBox2").query();
-//        clickOn(comboBox2);
-//        comboBox2.getSelectionModel().select(0);
-//
-//        ComboBox<String> comboBox3 = lookup("#comboBox3").query();
-//        clickOn(comboBox3);
-//        comboBox3.getSelectionModel().select(0);
-//
-//        clickOn("Dodaj");
-//
-//        TableView<Channel> table2 = (TableView<Channel>) lookup("#table2").query();
-//        assertNotNull(table2, "Tabela nije pronađena.");
-//
-//        ObservableList<Channel> items = table2.getItems();
-//        assertNotNull(items, "Podaci u tabeli ne smeju biti null.");
-//        assertTrue(items.size() > 0, "Tabela ne sadrži nove kanale.");
-//
-//        boolean found = items.stream().anyMatch(channel ->
-//                channel.getName().equals("Novi Kanal") &&
-//                        channel.getDescription().equals("Opis novog kanala"));
-//
-//        assertTrue(found, "Novi kanal nije dodat u tabelu.");
-//    }
+        clickOn(usernameField).write("mara");
+        clickOn(passwordField).write("mara123");
+        clickOn(loginButton);
+
+        TabPane tabPane = lookup("#tabPane").query();
+        tabPane.getSelectionModel().select(1);
+
+        TextField nameField = (TextField) lookup("#name").nth(0).query();
+        TextField descriptionField = (TextField) lookup("#description").nth(1).query();
+
+        clickOn(nameField).write("Novi Kanal");
+        clickOn(descriptionField).write("Opis novog kanala");
+
+        ComboBox<String> comboBox1 = lookup("#comboBox1").query();
+        clickOn(comboBox1);
+        type(KeyCode.DOWN);
+        type(KeyCode.ENTER);
+
+        ComboBox<String> comboBox2 = lookup("#comboBox2").query();
+        clickOn(comboBox2);
+        type(KeyCode.DOWN);
+        type(KeyCode.ENTER);
+
+        ComboBox<String> comboBox3 = lookup("#comboBox3").query();
+        clickOn(comboBox3);
+        type(KeyCode.DOWN);
+        type(KeyCode.ENTER);
+
+        clickOn("Dodaj");
+
+        TableView<Channel> table2 = (TableView<Channel>) lookup("#table2").query();
+        assertNotNull(table2, "Tabela nije pronađena.");
+
+        ObservableList<Channel> items = table2.getItems();
+        assertNotNull(items, "Podaci u tabeli ne smeju biti null.");
+        assertTrue(items.size() > 0, "Tabela ne sadrži nove kanale.");
+
+        boolean found = items.stream().anyMatch(channel ->
+                channel.getName().equals("Novi Kanal") &&
+                        channel.getDescription().equals("Opis novog kanala"));
+
+        assertTrue(found, "Novi kanal nije dodat u tabelu.");
+    }
+
+
+
 //
 //
 //    @Test
